411635a20affd5540a0e20f960b03ee33b23220d
pandya6988 <81331791+pandya6988@users.noreply.github.com>
2021-11-01 03:12:11 +0100

    Update code.py

--- colabcode/code.py
+++ colabcode/code.py
@@ -15,7 +15,7 @@ except ImportError:
     colab_env = False
 
 
-EXTENSIONS = ["ms-python.python", "ms-toolsai.jupyter", "mechatroner.rainbow-csv", "vscode-icons-team.vscode-icons, kiteco.kite"]
+EXTENSIONS = ["ms-python.python", "ms-toolsai.jupyter", "mechatroner.rainbow-csv", "vscode-icons-team.vscode-icons, "kiteco.kite"]
 CODESERVER_VERSION = "3.10.2"
 
 
