af9c9e10e193bbdd254afd3fa176561ccc47c39b
pandya6988 <81331791+pandya6988@users.noreply.github.com>
2021-11-01 04:05:31 +0100

    Update code.py

--- colabcode/code.py
+++ colabcode/code.py
@@ -15,7 +15,7 @@ except ImportError:
     colab_env = False
 
 
-EXTENSIONS = ["ms-python.python", "ms-toolsai.jupyter", "mechatroner.rainbow-csv", "vscode-icons-team.vscode-icons", "dongli.python-preview", "almenon.arepl"]
+EXTENSIONS = ["ms-python.python", "ms-toolsai.jupyter", "mechatroner.rainbow-csv", "vscode-icons-team.vscode-icons", "dongli.python-preview", "njpwerner.autodocstring"]
 CODESERVER_VERSION = "3.10.2"
 
 
